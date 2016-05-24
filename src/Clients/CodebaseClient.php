9eb7df7c4522dea0a4b1d67b91eeafaabed55130
Michael Peacock <mkpeacock@gmail.com>
2016-05-24 22:58:44 +0100

    Complete codebase client and add unit test coverage

--- src/Clients/CodebaseClient.php
+++ src/Clients/CodebaseClient.php
@@ -2,37 +2,81 @@
 
 namespace ParityBit\DeploymentNotifier\Clients;
 
+use Curl\Curl;
 use ParityBit\DeploymentNotifier\Deployment;
 
 class CodebaseClient
 {
-    protected $username;
-	protected $password;
+    protected $curl;
+    protected $codebaseProjectName;
+    protected $repositoryName;
 	protected $headers = [
-        'Accept: application/xml',
-        'Content-type: application/xml',
+        'Accept' => 'application/xml',
+        'Content-type' => 'application/xml',
     ];
 
 	const WEBSERVICE_URL = 'http://api3.codebasehq.com/';
 
-    public function __construct($username, $password)
+    public function __construct(Curl $curl, $username, $password, $codebaseProjectName, $repositoryName = null)
     {
-        $this->username = $username;
-        $this->password = $password;
+        $this->curl = $curl;
+        $this->codebaseProjectName = $codebaseProjectName;
+        $this->repositoryName = $repositoryName;
+        foreach ($this->headers as $header => $value) {
+            $this->curl->setHeader($header, $value);
+        }
+
+        $this->curl->setBasicAuthentication($username, $password);
     }
 
     public function getTicketsFromIds($ticketIds = [])
     {
+        $this->curl->get(
+            self::WEBSERVICE_URL . $this->codebaseProjectName . '/tickets',
+            [
+                'query' => 'id' . implode(',', $ticketIds)
+            ]
+        );
 
+        if ($this->curl->error) {
+            return [];
+        }
+        else {
+            $tickets = simplexml_load_string($this->curl->response);
+            $completed_ticket_names = [];
+            foreach($tickets as $ticket) {
+                $completed_ticket_names[] = (string) $ticket->summary;
+            }
+
+            return $completed_ticket_names;
+        }
     }
 
     public function updateTicketStatus($project, $ticketId, $newStatus)
     {
+        $payload = "<ticket-note>
+    <changes>
+        <status-id>" . $newStatus . "</status-id>
+    </changes>
+</ticket-note>";
 
+        $this->curl->post(
+            self::WEBSERVICE_URL . $this->codebaseProjectName . '/tickets/' . $ticketId . '/notes',
+            $payload
+        );
     }
 
     public function registerDeployment(Deployment $deployment)
     {
+        $payload = "<deployment>
+  <revision>" . (string) $deployment->getCurrentVersion() . "</revision>
+  <environment>" . (string) $deployment->getEnvironment() . "</environment>
+  <servers>" . (string) $deployment->getServer() . "</servers>
+</deployment>";
 
+        $this->curl->post(
+            self::WEBSERVICE_URL . $this->codebaseProjectName . '/' . $this->repositoryName . '/deployments',
+            $payload
+        );
     }
 }
---
+++ tests/Clients/CodebaseClientTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace ParityBit\DeploymentNotifier\Clients;
+
+use Curl\Curl;
+use ParityBit\DeploymentNotifier\Environment;
+use ParityBit\DeploymentNotifier\Server;
+use ParityBit\DeploymentNotifier\Version;
+
+
+use ParityBit\DeploymentNotifier\ChangeInspectors\ChangeInspector;
+use ParityBit\DeploymentNotifier\Deployment;
+
+class CodebaseClientTest extends \PHPUnit_Framework_TestCase
+{
+    protected $curl;
+    protected $codebaseClient;
+
+    public function setUp()
+    {
+        $this->faker = \Faker\Factory::create();
+
+        $this->curl = $this->getMockBuilder(Curl::class)
+                           ->setMethods(['post', 'get'])
+                           ->getMock();
+
+        $this->username = $this->faker->word;
+        $this->password = $this->faker->word;
+        $this->project = $this->faker->word;
+        $this->repo = $this->faker->word;
+
+        $this->buildClient();
+    }
+
+    protected function buildClient()
+    {
+        $this->codebaseClient = new CodebaseClient($this->curl, $this->username, $this->password, $this->project, $this->repo);
+    }
+
+    public function testConstructor()
+    {
+        $this->curl = $this->getMockBuilder(Curl::class)
+                           ->setMethods(['setHeader', 'setBasicAuthentication'])
+                           ->getMock();
+
+        $this->curl->expects($this->once())->method('setBasicAuthentication')->with($this->equalTo($this->username), $this->equalTo($this->password));
+        $this->curl->expects($this->exactly(2))->method('setHeader');
+
+        $this->buildClient();
+    }
+
+    public function testGetTicketsFromIdsOnFailure()
+    {
+        $ticketIds = [1,3,5];
+        $ticketData = ['Ticket ABCD', 'Another Ticket'];
+        $url = CodebaseClient::WEBSERVICE_URL . $this->project . '/tickets';
+        $payload = [
+            'query' => 'id' . implode(',', $ticketIds),
+        ];
+        $this->curl->response = $this->ticketResponse();
+        $this->curl->error = true;
+
+        $this->buildClient();
+
+        $data = $this->codebaseClient->getTicketsFromIds($ticketIds);
+        $this->assertEquals([], $data);
+    }
+
+    public function testGetTicketsFromIds()
+    {
+        $ticketIds = [1,3,5];
+        $ticketData = ['Ticket ABCD', 'Another Ticket'];
+        $url = CodebaseClient::WEBSERVICE_URL . $this->project . '/tickets';
+        $payload = [
+            'query' => 'id' . implode(',', $ticketIds),
+        ];
+        $this->curl->response = $this->ticketResponse();
+
+        $this->buildClient();
+
+        $data = $this->codebaseClient->getTicketsFromIds($ticketIds);
+        $this->assertEquals($ticketData, $data);
+    }
+
+    protected function ticketResponse()
+    {
+        return "<tickets>
+            <ticket>
+                <summary>Ticket ABCD</summary>
+            </ticket>
+            <ticket>
+                <summary>Another Ticket</summary>
+            </ticket>
+        </tickets>";
+    }
+
+    public function testUpdateTicketStatus()
+    {
+        $ticketId = $this->faker->numberBetween(1, 1000);
+        $newStatus = $this->faker->word;
+        $this->curl->expects($this->once())
+                   ->method('post')
+                   ->with(
+                        $this->equalTo(
+                            CodebaseClient::WEBSERVICE_URL  . $this->project . '/tickets/' . $ticketId . '/notes'
+                        ),
+                        $this->callback(function ($subject) use ($newStatus) {
+                            $data = simplexml_load_string($subject);
+                            return ($newStatus == $data[0]->changes->{'status-id'});
+                        })
+                    );
+
+        $this->buildClient();
+
+        $this->codebaseClient->updateTicketStatus($this->project, $ticketId, $newStatus);
+    }
+
+    public function testRegisterDeployment()
+    {
+        $environmentValue = $this->faker->word;
+        $environment = new Environment($environmentValue);
+        $previousVersion = new Version($this->faker->word);
+        $currentVersionValue = $this->faker->word;
+        $currentVersion = new Version($currentVersionValue);
+        $serverValue = $this->faker->word;
+        $server = new Server($serverValue);
+
+        $changeInspector = $this->getMockBuilder(ChangeInspector::class)
+                                ->disableOriginalConstructor()
+                                ->getMock();
+
+        $deployment = new Deployment(
+            $environment,
+            $previousVersion,
+            $currentVersion,
+            $server,
+            $changeInspector
+        );
+
+        $this->curl->expects($this->once())
+                   ->method('post')
+                   ->with(
+                        $this->equalTo(
+                            CodebaseClient::WEBSERVICE_URL . $this->project . '/' . $this->repo . '/deployments'
+                        ),
+                        $this->callback(function ($subject) use ($currentVersionValue, $environmentValue, $serverValue) {
+                            $data = simplexml_load_string($subject);
+
+                            if (
+                                $data->revision == $currentVersionValue &&
+                                $data->environment == $environmentValue &&
+                                $data->servers = $serverValue
+                            ) {
+                                return true;
+                            }
+
+                        })
+                   );
+
+        $this->buildClient();
+
+        $this->codebaseClient->registerDeployment($deployment);
+    }
+}
